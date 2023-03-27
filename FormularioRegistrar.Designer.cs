00013bcf8a31b95a0790d86aff301425618736c7
Aurelio <Aurelio@LAPTOP-AURE>
2023-03-27 10:50:00 -0600

    1.2.2

--- FormularioRegistrar.Designer.cs
+++ FormularioRegistrar.Designer.cs
@@ -28,120 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtUsuario = new System.Windows.Forms.TextBox();
-            this.txtContrasena = new System.Windows.Forms.TextBox();
-            this.txtCorreo = new System.Windows.Forms.TextBox();
-            this.btnCancelar = new System.Windows.Forms.Button();
-            this.btnRegistrar = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtUsuario = new TextBox();
+            txtContrasena = new TextBox();
+            txtCorreo = new TextBox();
+            btnCancelar = new Button();
+            btnRegistrar = new Button();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(184, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(434, 54);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Registro de Usuario:";
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(184, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(434, 54);
+            label1.TabIndex = 0;
+            label1.Text = "Registro de Usuario:";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(112, 123);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 19);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Usuario:";
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(112, 123);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Usuario:";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(87, 175);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 19);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Contraseña:";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(87, 175);
+            label3.Name = "label3";
+            label3.Size = new Size(91, 19);
+            label3.TabIndex = 2;
+            label3.Text = "Contraseña:";
+            label3.Click += label3_Click;
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(117, 232);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 19);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Correo:";
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(117, 232);
+            label4.Name = "label4";
+            label4.Size = new Size(61, 19);
+            label4.TabIndex = 3;
+            label4.Text = "Correo:";
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(184, 123);
-            this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(250, 23);
-            this.txtUsuario.TabIndex = 4;
+            txtUsuario.Location = new Point(184, 123);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(250, 23);
+            txtUsuario.TabIndex = 4;
+            txtUsuario.TextChanged += txtUsuario_TextChanged;
             // 
             // txtContrasena
             // 
-            this.txtContrasena.Location = new System.Drawing.Point(184, 175);
-            this.txtContrasena.Name = "txtContrasena";
-            this.txtContrasena.Size = new System.Drawing.Size(250, 23);
-            this.txtContrasena.TabIndex = 5;
+            txtContrasena.Location = new Point(184, 175);
+            txtContrasena.Name = "txtContrasena";
+            txtContrasena.Size = new Size(250, 23);
+            txtContrasena.TabIndex = 5;
+            txtContrasena.TextChanged += txtContrasena_TextChanged;
             // 
             // txtCorreo
             // 
-            this.txtCorreo.Location = new System.Drawing.Point(184, 228);
-            this.txtCorreo.Name = "txtCorreo";
-            this.txtCorreo.Size = new System.Drawing.Size(250, 23);
-            this.txtCorreo.TabIndex = 6;
+            txtCorreo.Location = new Point(184, 228);
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.Size = new Size(250, 23);
+            txtCorreo.TabIndex = 6;
+            txtCorreo.TextChanged += txtCorreo_TextChanged;
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnCancelar.Location = new System.Drawing.Point(481, 353);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(137, 42);
-            this.btnCancelar.TabIndex = 7;
-            this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCancelar.Location = new Point(481, 353);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(137, 42);
+            btnCancelar.TabIndex = 7;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click;
             // 
             // btnRegistrar
             // 
-            this.btnRegistrar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnRegistrar.Location = new System.Drawing.Point(184, 353);
-            this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(137, 42);
-            this.btnRegistrar.TabIndex = 8;
-            this.btnRegistrar.Text = "Registrar";
-            this.btnRegistrar.UseVisualStyleBackColor = true;
-            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            btnRegistrar.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnRegistrar.Location = new Point(184, 353);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(137, 42);
+            btnRegistrar.TabIndex = 8;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = true;
+            btnRegistrar.Click += btnRegistrar_Click;
             // 
             // FormularioRegistrar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnRegistrar);
-            this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.txtCorreo);
-            this.Controls.Add(this.txtContrasena);
-            this.Controls.Add(this.txtUsuario);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "FormularioRegistrar";
-            this.Text = "FormularioRegistrar";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 192, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnRegistrar);
+            Controls.Add(btnCancelar);
+            Controls.Add(txtCorreo);
+            Controls.Add(txtContrasena);
+            Controls.Add(txtUsuario);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FormularioRegistrar";
+            Text = "FormularioRegistrar";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
--- FormularioRegistrar.cs
+++ FormularioRegistrar.cs
@@ -24,7 +24,31 @@ namespace ProyectoCheques
 
         private void btnRegistrar_Click(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void txtUsuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtContrasena_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCorreo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hasta luego");
+            FormularioRegistrar reg = new FormularioRegistrar();
+            this.Close();   
+            Form1 formu = new Form1();
+            formu.ShowDialog();
         }
     }
 }
